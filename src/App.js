564d127a85a51ac846aa75a82d3dd3e9c51b514c
Aye <70176152+ayebrian@users.noreply.github.com>
2022-06-07 23:28:49 +0300

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { switchTheme } from "./js/okand.js";
+function App() {
+  return (
+    <div className="App">
+      <div className="root">
+        <nav className="header">
+          <div className="header-left">
+            <div className="header-left-text">FireCloud</div>
+          </div>
+          <div className="header-right">
+            <div className="header-right-text"></div>
+          </div>
+        </nav>
+        <div className="content">
+          <div className="container">
+            <div className="aboutme">
+              <div className="abt-top">
+                <div className="abt-text-top">About me</div>
+                <div className="abt-icons">
+                  <a href="https://github.com/firedcloud">
+                    <i className="bi bi-github"></i>
+                  </a>
+                  <a href="/help">
+                    <i className="bi bi-question-circle-fill"></i>
+                  </a>
+                </div>
+              </div>
+              <div className="abt-bottom">
+                krinje page tbh. i dont know what to write here. i'll stay this
+                cringe in repo.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
