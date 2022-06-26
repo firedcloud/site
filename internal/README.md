e3a83efbd6be3fdcf1c0c140f70f82c72c89ff4e
Aye <70176152+ayebrian@users.noreply.github.com>
2022-06-26 18:24:41 +0300

    iMad

--- internal/README.md
+++ internal/README.md
@@ -1,8 +1,13 @@
+---
+title: Internal Release
+date: N/A
+---
 # This is internal release for our clients inside of our company.
 # DO NOT USE IT IN YOUR CODE.
 # It is only for our internal usage.
 # ACCESS PROHIBITED.
 
-    
-    []: # Language: markdown
-    []: # Path: internal\README.md  
+## What is this?
+<P>This is internal release for our clients inside of our company.
+<P>DO NOT USE IT IN YOUR CODE.
+<button style="background: white; border: 0; padding:5px 20px; border-radius: 5px;" href="/int/login">Very good</button>
---
+++ internal/rmt_raid_core.js
@@ -0,0 +1,24 @@
+// connecting
+
+import { exec } from "child_process";
+import { connect } from "mongoose";
+
+// --------------------------------------------------------------------------------------------------------------------
+const ip = '10.59.1.23';
+const domain = 'storagepool';
+const country = 'de';
+const username = 'storage_connect_56487';
+const password = 'S56809T4PRX^&!>gffty5894*&%^:?>ndgklJGyu3r087tn319r3468go3269r42$%';
+const pool = 'storagepool28';
+const pool_id = '28';
+
+const connectionrmt = connect(connectionrmt = connect(`mongodb://${ip}/${domain}`));
+connect(connectionrmt);
+export default rmt_raid_core;
+const bin_shell = require('child_process').spawn('/bin/bash');
+const command = `sshpass -p ${password} ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 22 ${username}@${ip}`;
+const bin_shell_command = `${command}`;
+const bin_shell_command_2 = `${command} -s /bin/bash`;
+const bin_shell_command_3 = `${command} -s /bin/sh`;
+
+exec();
