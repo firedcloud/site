616a3d4ec74f9645754a0eec9e64a798f0f373ae
Aye <70176152+ayebrian@users.noreply.github.com>
2022-06-27 01:06:24 +0300

    added apos and quot replacing

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server_ip = "0.0.0.0";
 app.use(express.static(path.join(__dirname, "static_local")));
 app.get("/", (req, res) => {
   // read the file and sanitize the html
-  const host_file = fs.readFileSync("./server_hostname.txt", "utf8").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+  const host_file = fs.readFileSync("./server_hostname.txt", "utf8").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/'/g, "&apos;").replace(/"/g, "&quot;");
   
   const css_test = fs.readFileSync(
     path.join(__dirname, "static_local/css/test.css"),
@@ -31,15 +31,13 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.post("/", urlencodedParser, (req, res) => {
   //setting new hostname
   if (req.body.hostname) {
-    if (/[^a-zA-Z0-9]/.test(req.body.hostname)) {
+    if (/[^a-zA-Zа-яА-Я0-9]/.test(req.body.hostname)) {
       return res.send("Only letters and numbers!");
     }
     if (req.body.hostname.length > 20) {
       return res.send("Too long!");
     }
-
     // if contains special characters then return error
-
     fs.writeFileSync("./server_hostname.txt", req.body.hostname);
     return res.send("Hostname set");
   } else {
