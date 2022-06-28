dc62aafa512e0a5b0e658561cf482e074eae57ad
Aye <70176152+ayebrian@users.noreply.github.com>
2022-06-28 21:11:05 +0300

    sssy

--- node_modules/@nuxt/vue-app/template/components/nuxt-link.client.js
+++ node_modules/@nuxt/vue-app/template/components/nuxt-link.client.js
@@ -17,7 +17,7 @@ const cancelIdleCallback = window.cancelIdleCallback || function (id) {
 
 const observer = window.IntersectionObserver && new window.IntersectionObserver((entries) => {
   entries.forEach(({ intersectionRatio, target: link }) => {
-    if (intersectionRatio <= 0) {
+    if (intersectionRatio <= 0 || !link.__prefetch) {
       return
     }
     link.__prefetch()
@@ -111,7 +111,8 @@ export default {
       // Preload the data only if not in preview mode
       if (!this.$root.isPreview) {
         const { href } = this.$router.resolve(this.to, this.$route, this.append)
-        this.$nuxt.fetchPayload(href).catch(() => {})
+        if (this.<%= globals.nuxt %>)
+          this.<%= globals.nuxt %>.fetchPayload(href, true).catch(() => {})
       }
       <% } %>
       <% if (router.linkPrefetchedClass) { %>
